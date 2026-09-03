0722cfcd388cd6d7248285854e6219e1602f2af2
dafm10 <dafm.10@gmail.com>
2026-09-02 23:05:18 -0500

    chore: ejerccios POO herencia

--- semana03/05-POO-herencia.py
+++ semana03/05-POO-herencia.py
@@ -64,3 +64,80 @@ g.araniar()
 p.dormir()
 p.ladrar()
 p.comer()
+
+
+# 1. Crear una clase padre llamada Figura y dos clases hijos llamada Cuadrado y Triángulo. La clase Figura tiene el atributo nombre y un método área() que retorna 0 por defecto. La clase Cuadrado tiene un atributo adicional que es lado y la clase Triángulo tiene atributo base y altura, ambas clases heredan de Figura y se necesita sobreescribir el método area()
+
+class Figura:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def area():
+        return 0
+
+class Cuadrado(Figura):
+    def __init__(self, nombre, lado):
+        super().__init__(nombre)
+        self.lado = lado
+
+    def area(self):
+        return self.lado ** 2
+
+class Triangulo(Figura):
+    def __init__(self, nombre, base, altura):
+        super().__init__(nombre)
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return (self.base * self.altura) / 2
+
+c = Cuadrado("Cuadrado", 5)
+print(c.area())
+
+t = Triangulo("Triangulo", 12, 4)
+print(t.area())
+
+
+# 2. Clase Persona y clase Guerrera y Mago, en la cual Persona tiene nombre y vida (valor predeterminaro es 100) y un método recibir_danio(cantidad) que resta vida hasta llegar a 0. Guerrero tiene el atributo fuerza y un método atacar() que imprime el daño causado según la fuerza que tenga. La clase Mago tiene atributo maná y un método lanza_hechizo() que solo funciona si tiene suficiente maná (si no muestra un mensaje de error)
+
+class Persona:
+    def __init__(self, nombre, vida = 100):
+        self.nombre = nombre
+        self.vida = vida
+
+    def recibir_danio(self, cantidad):
+        if self.vida - cantidad < 0 :
+            return "Ya se murió"
+        
+        self.vida -= cantidad
+
+class Guerrero(Persona):
+    def __init__(self, nombre, fuerza, vida = 100):
+        super().__init__(nombre, vida)
+        self.fuerza = fuerza
+
+    def atacar(self):
+        return f"Has hecho {self.fuerza} puntos de daño"
+
+class Mago(Persona):
+    def __init__(self, nombre, mana, vida=100):
+        super().__init__(nombre, vida)
+        self.mana = mana
+
+    def lanza_hechizo(self, costo_de_mana):
+        if costo_de_mana > self.mana :
+            return "No se puede lanzar el hechizo por falta de maná!"
+
+        return "Lanzando el hechizo"
+
+persona = Persona("Anakin", 80)
+print(persona.recibir_danio(110))
+
+persa = Guerrero("Leonidas", 100)
+print(persa.atacar())
+
+merlin = Mago("Merlin", 50, 70)
+merlin.recibir_danio(20)
+print(merlin.lanza_hechizo(100))
+print(merlin.lanza_hechizo(40))
