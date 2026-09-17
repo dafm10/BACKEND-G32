5721cdb39cd88a47503b7d5ffa8a5098b256cebc
dafm10 <dafm.10@gmail.com>
2026-09-17 11:47:12 -0500

    feat: uso de query strings en python con DB

--- semana05/app.py
+++ semana05/app.py
@@ -1,5 +1,16 @@
 # from LIBRERIA import CLASES ó FUNCIONES que queremos usar de la librería.
 from flask import Flask, request
+from werkzeug.exceptions import UnsupportedMediaType
+from psycopg import connect # connect > para conectarse a la base de datos
+from dotenv import load_dotenv
+from os import environ # os > operating systems # environ devolverá todas las variables de entorno de la máquina y aqui se agregarán las variables del archivo .env
+
+# load_dotenv siempre debe ir en la primera linea del prouecto para que cargue todas las variables en todo el proyecto y evitar alguna variable no leida
+load_dotenv()
+
+# postgresql://NOMBRE_USUARIO:PASSWORD_USUARIO@HOST:PUERTO/NOMBRE_BD
+credenciales = environ.get("DATABASE_URL")
+conexion = connect(conninfo=credenciales)
 # request > nos dará toda la información proveniente del cliente, y solamente puede ser llamado dentro de un controlador
 # https://flask.palletsprojects.com/en/stable/api/#flask.Request
 
@@ -34,15 +45,71 @@ def estado_servidor():
 def gestionar_productos():
     print(request.method)
     if request.method == 'GET':
-    # Los controladores (es la lógica del endpoint) suelen retornar diccionarios que estos serán interpretados en JSON o también se suele retornar listas (arreglos)
+        # El encargado de iniciar la comunicación con la BD
+        cursor = conexion.cursor()
+
+        # Ejecutamos el comando en la BD
+        cursor.execute("SELECT * FROM productos")
+
+        #Para obtener el resultado (si es necesario) usamos los métodos: fetchone, fecthall, fetchmany
+        productos_bd = cursor.fetchall()
+
+        print(productos_bd)
+
+        # Finaliza la comunicación con la base de datos
+        cursor.close
+
+        resultado = []
+        for producto in productos_bd:
+            resultado.append({
+                "id": producto[0],
+                "nombre": producto[1],
+                "precio": float(producto[2]),
+                "cantidad": producto[3]
+            })
+        
+        # Los controladores (es la lógica del endpoint) suelen retornar diccionarios que estos serán interpretados en JSON o también se suele retornar listas (arreglos)
         return {
             "message": "Los productos son:",
-            "content": productos
+            "content": resultado
         }
     elif request.method == 'POST':
-        return {
-            "meesage": "Producto creado exitosamente"
-        }
+        # Se usa para crear nueva información proveniente del frontend
+        # request.get_data() # retorna la información proveniente del cliente directamente como si fuese un string. Sirve también si la información enviada será un texto. No se recomienda mucho usarlo por que tenemos que volver a String y parsear para que sea un diccionario y validarlo para ver si cumple con las llaves
+        # request.get_json() # retorna la información del cliente y la convierte a un diccionario pra que python la pueda entender
+        print(request.get_data())
+        try:
+            # print(request.get_json())
+            data = request.get_json()
+
+            cursor = conexion.cursor()
+
+            # El %s hace la conversión de la información proveniente del cliente a un String sin parámetros que puedan vulnerar mi base de datos, y ...
+            # ... en los Strings comunes podemos utilizar %f para flotantes y adicionalmente el %i para convertir a enteros y podemos evitar ataques directos a la base de datos (SQL INYECTION)
+            # Si queremos retornar la información que acabamos de granar en la BD, se puede utilizar el comando RETURNING columnas, es decir, si ponemos iNSERT INTO ... VALUES ... RETURNING *, esto devolverá toda la información agregada a la BD
+            # https://www.psycopg.org/psycopg3/docs/basic/params.html
+            cursor.execute("INSERT INTO productos (nombre, precio, cantidad) VALUES (%s, %s, %s) RETURNING *",(
+                data.get("nombre"), 
+                data.get("precio"),
+                data.get("cantidad")))
+
+            # para asegurar la data y asegurarnos de que se guarde la información de manera peramente en la bd
+            conexion.commit()
+
+            # para obtener el nuevo producto creado
+            nuevo_producto = cursor.fetchone()
+
+            print(nuevo_producto)
+
+            cursor.close()
+            return {
+                        "meesage": "Producto creado exitosamente"
+                    }
+        except UnsupportedMediaType: # Exception es la clase primordial de todos los errores
+            # Handler (manejador de errores)
+            return {
+                "message": "Debes enviar la información en formato JSON"
+            }
 
 if __name__ == "__main__":
     # el método RUN ejecuta el servidor y lo mantiene escuchando peticiones
